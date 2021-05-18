3723d77301989563181c278dd3fad4ff1918b971
kane-young <lyw2100@naver.com>
2021-05-18 16:57:07 +0900

    Feat : HTTPMethod, URL로 사용할 enum 지정

---
+++ OpenMarket/OpenMarket/Network/APIRequestType.swift
@@ -0,0 +1,37 @@
+//
+//  APIRequestType.swift
+//  OpenMarket
+//
+//  Created by 이영우 on 2021/05/18.
+//
+
+import Foundation
+
+enum APIRequestType {
+  static let baseURL: String = "https://camp-open-market-2.herokuapp.com"
+
+  case loadPage(page: Int)
+  case loadProduct(id: Int)
+  case postProduct
+  case patchProduct(id: Int)
+  case deleteProduct(id: Int)
+
+  var urlPath: String {
+    switch self {
+    case .loadPage(let page):
+      return "/items/\(page)"
+    case .loadProduct(let id):
+      return "/item/\(id)"
+    case .postProduct:
+      return "/item/"
+    case .patchProduct(let id):
+      return "/item/\(id)"
+    case .deleteProduct(let id):
+      return "/item/\(id)"
+    }
+  }
+  
+  var url: URL? {
+      return URL(string: "\(APIRequestType.baseURL)\(urlPath)")
+  }
+}
---
+++ OpenMarket/OpenMarket/Network/HTTPMethod.swift
@@ -0,0 +1,38 @@
+//
+//  HTTPMethod.swift
+//  OpenMarket
+//
+//  Created by 이영우 on 2021/05/18.
+//
+
+import Foundation
+
+enum HTTPMethod: CustomStringConvertible {
+  case get, post, put, patch, delete
+  
+  var description: String {
+    switch self {
+    case .get:
+      return "GET"
+    case .post:
+      return "POST"
+    case .put:
+      return "PUT"
+    case .patch:
+      return "PATCH"
+    case .delete:
+      return "DELETE"
+    }
+  }
+  
+  static let contentType = "Context-Type"
+  
+  func makeHeaderContextType(boundary: String) -> String {
+    switch self {
+    case .get, .delete, .put:
+      return "application/json"
+    case .patch, .post:
+      return "multipart/form-data; boundary=\(boundary)"
+    }
+  }
+}
