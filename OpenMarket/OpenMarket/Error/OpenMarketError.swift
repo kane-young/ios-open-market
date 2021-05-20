a1da5210968de314686f66690c6325ff1ef920f1
kane-young <lyw2100@naver.com>
2021-05-20 11:00:10 +0900

    Feat : 에러 타입을 통해 에러 상황 명시

--- OpenMarket/OpenMarket/Error/OpenMarketError.swift
+++ OpenMarket/OpenMarket/Error/OpenMarketError.swift
@@ -7,7 +7,28 @@
 
 import Foundation
 
-enum OpenMarketError: Error {
-  case invalidSearchResult
+enum OpenMarketError: Error, CustomStringConvertible {
   case decodingProblem
+  case encodingProblem
+  case invalidURL
+  case connectionProblem
+  case invalidData
+  case invalidRequest
+  
+  var description: String {
+    switch self {
+    case .decodingProblem:
+      return "Decoding에 실패했습니다"
+    case .encodingProblem:
+      return "Encoding에 실패했습니다"
+    case .invalidURL:
+      return "유효하지 않는 URL입니다"
+    case .connectionProblem:
+      return "연결에 실패했습니다"
+    case .invalidData:
+      return "유효하지 않은 데이터입니다"
+    case .invalidRequest:
+      return "유효하지 않은 요청입니다"
+    }
+  }
 }
