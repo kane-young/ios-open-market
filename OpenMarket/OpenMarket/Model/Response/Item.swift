66abfc114880ba5dfdbcea288db9005d37ac25ab
kane-young <lyw2100@naver.com>
2021-08-24 23:14:47 +0900

    refactor: json decoding 할 Item, ItemList에서 init 제거 및 case 이름 수정

--- OpenMarket/OpenMarket/Model/Response/Item.swift
+++ OpenMarket/OpenMarket/Model/Response/Item.swift
@@ -20,22 +20,8 @@ struct Item: Decodable {
   let registrationDate: Double
   
   private enum CodingKeys: String, CodingKey {
-    case id, title, description, price, currency, stock, thumbnails, images
+    case id, title, descriptions, price, currency, stock, thumbnails, images
     case discountedPrice = "discounted_price"
     case registrationDate = "registration_date"
-  }
-  
-  init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    self.id = (try? container.decode(Int.self, forKey: .id)) ?? 0
-    self.title = (try? container.decode(String.self, forKey: .title)) ?? ""
-    self.descriptions = (try? container.decode(String.self, forKey: .title)) ?? ""
-    self.price = (try? container.decode(Int.self, forKey: .price)) ?? 0
-    self.currency = (try? container.decode(String.self, forKey: .currency)) ?? ""
-    self.stock = (try? container.decode(Int.self, forKey: .stock)) ?? 0
-    self.discountedPrice = (try? container.decode(Int?.self, forKey: .discountedPrice)) ?? 0
-    self.thumbnails = (try? container.decode([String].self, forKey: .thumbnails)) ?? []
-    self.images = (try? container.decode([Data].self, forKey: .thumbnails)) ?? []
-    self.registrationDate = (try? container.decode(Double.self, forKey: .registrationDate)) ?? 0.0
   }
 }
